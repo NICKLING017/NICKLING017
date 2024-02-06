6b3f2e7d619bb9ac31001582e4c6df9434c5812c
NICKLING017 <326914560@qq.com>
2024-02-06 16:25:33 +0800

    lab00

---
+++ CS61A/lab00/lab00.py
@@ -0,0 +1,12 @@
+def twenty_twenty_four():
+    """Come up with the most creative expression that evaluates to 2024
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_four()
+    2024
+    """
+    output = 2 * 1000 + 2 * 10 + 4
+    return output
+
+
+print(twenty_twenty_four())
