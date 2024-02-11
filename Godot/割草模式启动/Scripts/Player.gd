b746f061a4f0399064f2672ba5fab30355e318cf
NICKLING017 <326914560@qq.com>
2024-02-12 00:37:04 +0800

    添加冰矛子弹功能

--- Godot/割草模式启动/Scripts/Player.gd
+++ Godot/割草模式启动/Scripts/Player.gd
@@ -3,6 +3,27 @@ extends CharacterBody2D
 var movement_speed = 400.0
 var hp = 80
 
+# Attacks
+var iceSpear = preload("res://tscn/Player/Attack/ice_spear.tscn")
+
+# AttackNodes
+@onready var iceSpearTimer = $Attack/IceSpearTimer
+@onready var iceSpearAttackTimer = $Attack/IceSpearTimer/IceSpearAttackTimer
+
+# IceSpear
+var icespear_ammo = 0
+var icespear_baseammo = 1
+var icespear_attackspeed = 1.5
+var icespear_level = 1
+
+# Enemy Related
+var enemy_close = []
+
+func _ready():
+	attack()
+
+
+
 func _physics_process(delta):
 	movement()
 
@@ -24,7 +45,53 @@ func animateSprite(x_mov):
 		$AnimatedSprite2D.animation = "player_left"
 
 
+func attack():
+	if icespear_level > 0:
+		iceSpearTimer.wait_time = icespear_attackspeed
+		if iceSpearTimer.is_stopped():
+			iceSpearTimer.start()
 
 func _on_hurt_box_hurt(damage):
 	hp -= damage
 	print(hp)
+
+
+func _on_ice_spear_timer_timeout():
+	icespear_ammo += icespear_baseammo
+	iceSpearAttackTimer.start()
+
+
+func _on_ice_spear_attack_timer_timeout():
+	if icespear_ammo > 0:
+		# iceSpear.instantiate() 实例化iceSpear场景
+		var icespear_attack = iceSpear.instantiate()
+		icespear_attack.position = position
+		icespear_attack.target = get_random_target()
+		icespear_attack.level = icespear_level
+		add_child(icespear_attack)
+		icespear_ammo -= 1
+		if icespear_ammo > 0:
+			iceSpearAttackTimer.start()
+		else:
+			iceSpearAttackTimer.stop()
+
+
+
+
+func get_random_target():
+	if enemy_close.size() > 0:
+		var potential_target = enemy_close.pick_random()
+		if is_instance_valid(potential_target):
+			return potential_target.global_position
+	return Vector2.UP
+
+
+
+func _on_enemy_detection_area_body_entered(body):
+	if not enemy_close.has(body):
+		enemy_close.append(body)
+
+
+func _on_enemy_detection_area_body_exited(body):
+	if not enemy_close.has(body):
+		enemy_close.erase(body)
