83e585a3e38a59fe0346f6b0bbf678d14d3cb27f
NICKLING017 <326914560@qq.com>
2024-02-12 16:19:44 +0800

    怪物击退效果

--- Godot/割草模式启动/Scripts/enemy.gd
+++ Godot/割草模式启动/Scripts/enemy.gd
@@ -2,17 +2,26 @@ extends CharacterBody2D
 
 @export var movement_speed = 200.0
 @export var hp = 10
+@export var knockback_recovery = 3.5
+var knockback = Vector2.ZERO
 # 访问player组中的第一个节点
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var sprite = $Sprite2D
 @onready var anim = $AnimationPlayer
+@onready var snd_hit = $snd_hit
+
+var death_anim = preload("res://tscn/Enemy/explosion.tscn")
+
+signal remove_from_array(object)
 
 func _ready():
 	anim.play("walk")
 
 func _physics_process(_delta):
+	knockback = knockback.move_toward(Vector2.ZERO,knockback_recovery)
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * movement_speed
+	velocity += knockback
 	move_and_slide()
 	Sprite(direction)
 
@@ -23,7 +32,19 @@ func Sprite(direction):
 	else: sprite.flip_h = false
 
 
-func _on_hurt_box_hurt(damage):
+func death():
+	emit_signal("remove_from_array",self)
+	var enemy_death = death_anim.instantiate()
+	enemy_death.scale = sprite.scale
+	enemy_death.global_position  = global_position
+	get_parent().call_deferred("add_child",enemy_death)
+	queue_free()
+
+func _on_hurt_box_hurt(damage,angle,knockback_amount):
 	hp -= damage
+	# 击退距离
+	knockback = angle * knockback_amount * 2
 	if hp <=0:
-		queue_free()
+		death()
+	else :
+		snd_hit.play()
