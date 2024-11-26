fd197959ac62e995f4e9c38eb1f10c2c92641d35
NICKLING017 <326914560@qq.com>
2024-11-26 22:04:55 +0800

    死与新生

--- Godot/First Game/scripts/coin.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-
-
-func _on_body_entered(body):
-	print("+1 coin!")
-	queue_free() # 将节点加入队列，在当前帧结束时删除
--- Godot/基本训练/Bullet/bullet.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-
-var dir = Vector2.ZERO
-var speed = 2000
-var hurt = 1
-
-func _process(delta):
-	velocity =dir * speed
-	move_and_slide()
